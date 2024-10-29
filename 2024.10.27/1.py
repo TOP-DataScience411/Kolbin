0f4f8ba2b2ce15aebab4282abdc9b18648f75350
Ivan <sloviane@mail.ru>
2024-10-29 09:09:46 +0400

    1 задание

---
+++ 2024.10.27/1.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+name = input("Введите имя: ")
+surname = input("Введите фамилию: ")
+year = input("Введите год рождения: ")
+
+print(surname, name + ",", datetime.now().year - int(year))
+
+
+
+# C:\My\Учеба\Kolbin
+ # 13:41:26 > python 1.py
+# Введите имя: Ivan
+# Введите фамилию: Kolbin
+# Введите год рождения: 1991
+# Kolbin Ivan, 33
