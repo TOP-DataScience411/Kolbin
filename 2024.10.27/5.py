8bcc6c6a3db7d0b9207e5c6a87097aebe5de4c26
Ivan <sloviane@mail.ru>
2024-10-29 09:10:28 +0400

    5 задание

---
+++ 2024.10.27/5.py
@@ -0,0 +1,14 @@
+int_part = input()
+fract_part = input()
+
+miles = float(int_part + "." + fract_part)
+
+print(f"{miles} миль = {miles * 1.61:.1f} км")
+
+
+
+# C:\My\Учеба\Kolbin
+ # 18:26:05 > python 5.py
+# 15
+# 7
+# 15.7 миль = 25.3 км
