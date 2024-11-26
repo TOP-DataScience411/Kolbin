4df4dc462fba0293c381fd4162d3c1e6c81a6e7d
Ivan <sloviane@mail.ru>
2024-11-26 09:30:58 +0400

    3 задание

---
+++ 2024.11.24/3.py
@@ -0,0 +1,15 @@
+def tree_leaves(branch_list: list) -> int:
+    num = 0
+    for branch in branch_list:
+        if type(branch) is list:
+            num += tree_leaves(branch)
+        elif type(branch) is str:
+            num += 1
+    return num
+    
+tree = [[[['leaf', 'leaf', 'leaf', 'leaf'], 'leaf', 'leaf', 'leaf'], [['leaf', 'leaf'], 'leaf', 'leaf'], ['leaf', 'leaf', 'leaf']], [['leaf', 'leaf'], ['leaf', 'leaf', 'leaf', 'leaf', 'leaf', 'leaf'], 'leaf', 'leaf', 'leaf'], [['leaf'], ['leaf', 'leaf', ['leaf', 'leaf', 'leaf']], 'leaf', 'leaf'], ['leaf', 'leaf', ['leaf', 'leaf'], 'leaf']]
+
+
+
+# >>> tree_leaves(tree)
+# 38
