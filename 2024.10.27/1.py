038ac73ded8a3941655daaa6ee26b77ea030c4ad
Gennadiy <renardes@gmail.com>
2024-11-28 04:15:03 +0500

    2024.10.27: комментарии, правки, итоги

--- 2024.10.27/1.py
+++ 2024.10.27/1.py
@@ -7,10 +7,13 @@ year = input("Введите год рождения: ")
 print(surname, name + ",", datetime.now().year - int(year))
 
 
-
 # C:\My\Учеба\Kolbin
  # 13:41:26 > python 1.py
 # Введите имя: Ivan
 # Введите фамилию: Kolbin
 # Введите год рождения: 1991
 # Kolbin Ivan, 33
+
+
+# ИТОГ: отлично — 4/4
+
